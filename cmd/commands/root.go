463b176c950ec154a360c0da7b5e9bede8ee83ea
Eric_Ren <dellnoantechnp@users.noreply.github.com>
2025-10-21 15:47:12 +0800

    update: update code.

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -4,20 +4,8 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package commands
 
 import (
-	"RedisScanTask/Tasks"
-	"RedisScanTask/pkg/TaskError"
-	"context"
-	"fmt"
-	"github.com/redis/go-redis/v9"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"log"
-	"log/slog"
 	"os"
-	"regexp"
-	"strconv"
-	"sync"
-	"time"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -41,38 +29,6 @@ func Execute() {
 		os.Exit(1)
 	}
 
-	var programLevel = new(slog.LevelVar)
-	myLogger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel}))
-
-	initConfig()
-	address := viper.GetString("address")
-	password := viper.GetString("password")
-	pattern := viper.GetString("pattern")
-
-	start := time.Now()
-
-	client := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{
-			address,
-		},
-		Password:     password,
-		DialTimeout:  time.Second * time.Duration(viper.GetInt("dial_timeout")),
-		ReadTimeout:  time.Second * 10,
-		WriteTimeout: time.Second * 10,
-	})
-
-	client.Ping(context.Background())
-
-	keys, err := getAllKeysMatched(
-		context.WithValue(context.Background(), "logger", myLogger),
-		client,
-		pattern)
-	if err != nil {
-		panic(err)
-	}
-
-	elasped := time.Now().Sub(start)
-	fmt.Printf("Time elasped to SCAN all keys: %s, keys count: %+v\n", elasped, len(keys))
 }
 
 func init() {
@@ -88,74 +44,4 @@ func init() {
 
 	// 初始化配置文件
 	cobra.OnInitialize(initConfig)
-}
-
-func getAllKeysMatched(ctx context.Context, client *redis.ClusterClient, pattern string) (keys []string, err error) {
-	log.Printf("scanning for all the keys matched with: %s\n", pattern)
-
-	nodeIndex := 0
-	// 通过 ctx 获取logger
-	logger := ctx.Value("logger").(*slog.Logger)
-
-	// 利用 waitGroup 保证每个 master 节点的最终 total 日志保持最后输出
-	wg := sync.WaitGroup{}
-
-	err = client.ForEachMaster(ctx, func(ctx context.Context, rd *redis.Client) error {
-		nodeIndex++
-		c := 0
-		wg.Add(1)
-		addr := rd.String()
-
-		rePattern := "Redis<(.*) db.*"
-		re, err := regexp.Compile(rePattern)
-		redisNodeRe := re.FindStringSubmatch(addr)
-		if err != nil {
-			return err
-		}
-
-		redisNode := redisNodeRe[1]
-
-		logger2 := logger.With("node", redisNode)
-		logger2.Info("scan master node #" + strconv.Itoa(nodeIndex))
-		iter := client.Scan(ctx, 0, pattern, 1000).Iterator()
-
-		//var task *MemStats
-		task := &Tasks.MemStats{
-			LogSize:   400,
-			TaskError: TaskError.TaskError{Code: 200},
-		}
-
-		for iter.Next(ctx) {
-			key := iter.Val()
-			logger2.Info("found key", "name", key)
-			keys = append(keys, key)
-			c++
-			if s, err := task.Task(client, ctx, key); task.Code >= 500 {
-				panic(err)
-			} else if task.Code == 200 {
-				logger2.Info(s)
-			}
-		}
-		wg.Done()
-
-		// 打印最终 total 统计日志
-		wg.Wait()
-
-		// 每个 master 节点打印最终符合 pattern 名称key的总和
-		logger2.Info("total keys " + strconv.Itoa(c))
-
-		if err := iter.Err(); err != nil {
-			slog.Error("scan iterator has failed: %s", err)
-		}
-
-		return iter.Err()
-	})
-
-	if err != nil {
-		log.Printf("scanning redis cluster nodes with iterator has failed: %s", err)
-
-		return nil, err
-	}
-
-	return keys, nil
 }
--- cmd/commands/task.go
+++ cmd/commands/task.go
@@ -4,7 +4,19 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package commands
 
 import (
+	"RedisScanTask/Tasks"
+	"RedisScanTask/pkg/TaskError"
+	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
+	"log"
+	"log/slog"
+	"os"
+	"regexp"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -20,7 +32,7 @@ var taskCmd = &cobra.Command{
 			This application is a tool to generate the needed files
 			to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("task called")
+		run()
 	},
 }
 
@@ -36,4 +48,109 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func run() {
+	var programLevel = new(slog.LevelVar)
+	myLogger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: programLevel}))
+
+	initConfig()
+	address := viper.GetString("address")
+	password := viper.GetString("password")
+	pattern := viper.GetString("pattern")
+
+	start := time.Now()
+
+	client := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{
+			address,
+		},
+		Password:     password,
+		DialTimeout:  time.Second * time.Duration(viper.GetInt("dial_timeout")),
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	})
+
+	client.Ping(context.Background())
+
+	keys, err := getAllKeysMatched(
+		context.WithValue(context.Background(), "logger", myLogger),
+		client,
+		pattern)
+	if err != nil {
+		panic(err)
+	}
+
+	elapsed := time.Now().Sub(start)
+	fmt.Printf("Time elapsed to SCAN all keys: %s, keys count: %+v\n", elapsed, len(keys))
+}
+
+func getAllKeysMatched(ctx context.Context, client *redis.ClusterClient, pattern string) (keys []string, err error) {
+	log.Printf("scanning for all the keys matched with: %s\n", pattern)
+
+	nodeIndex := 0
+	// 通过 ctx 获取logger
+	logger := ctx.Value("logger").(*slog.Logger)
+
+	// 利用 waitGroup 保证每个 master 节点的最终 total 日志保持最后输出
+	wg := sync.WaitGroup{}
+
+	err = client.ForEachMaster(ctx, func(ctx context.Context, rd *redis.Client) error {
+		nodeIndex++
+		c := 0
+		wg.Add(1)
+		addr := rd.String()
+
+		rePattern := "Redis<(.*) db.*"
+		re, err := regexp.Compile(rePattern)
+		redisNodeRe := re.FindStringSubmatch(addr)
+		if err != nil {
+			return err
+		}
+
+		redisNode := redisNodeRe[1]
+
+		logger2 := logger.With("node", redisNode)
+		logger2.Info("scan master node #" + strconv.Itoa(nodeIndex))
+		iter := client.Scan(ctx, 0, pattern, 1000).Iterator()
+
+		//var task *MemStats
+		task := &Tasks.MemStats{
+			LogSize:   400,
+			TaskError: TaskError.TaskError{Code: 200},
+		}
+
+		for iter.Next(ctx) {
+			key := iter.Val()
+			logger2.Info("found key", "name", key)
+			keys = append(keys, key)
+			c++
+			if s, err := task.Task(client, ctx, key); task.Code >= 500 {
+				panic(err)
+			} else if task.Code == 200 {
+				logger2.Info(s)
+			}
+		}
+		wg.Done()
+
+		// 打印最终 total 统计日志
+		wg.Wait()
+
+		// 每个 master 节点打印最终符合 pattern 名称key的总和
+		logger2.Info("total keys " + strconv.Itoa(c))
+
+		if err := iter.Err(); err != nil {
+			slog.Error("scan iterator has failed: %s", err)
+		}
+
+		return iter.Err()
+	})
+
+	if err != nil {
+		log.Printf("scanning redis cluster nodes with iterator has failed: %s", err)
+
+		return nil, err
+	}
+
+	return keys, nil
 }
